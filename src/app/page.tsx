d4781529e23e7d821104e425607607826f893b41
Travis Carona <tcarona@gmail.com>
2025-04-08 21:34:48 -0500

    test new page routes for iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import ClientComponent from "./ClientComponent"
+
 export const dynamic = 'force-dynamic'
 
 async function getServerSideProps() {
@@ -17,28 +19,4 @@ export default async function Home() {
   return (
     <ClientComponent apiKey={apiKey} companyId={companyId} />
   )
-}
-
-"use client";
-import useHomekynd from "./useHomekynd";
-
-// The company might want to put this in a .env file  //
-const HOMEKYND_API_KEY = process.env.HOMEKYND_API_KEY ?? (() => { throw new Error('HOMEKYND_API_KEY is required') })()
-const HOMEKYND_COMPANY_ID = process.env.HOMEKYND_COMPANY_ID ?? (() => { throw new Error('HOMEKYND_COMPANY_ID is required') })()
-////////////////////////////////////////////////////////
-
-function ClientComponent({ apiKey, companyId }) {
-  const token = useHomekynd(companyId, apiKey)
-
-  return (
-    <div>
-      <h1>There is an iframe below</h1>
-      <iframe
-        src={`https://${process.env.NEXT_PUBLIC_HOMEKYND_COMPANY_URL}/?token=${token}`}
-        width="100%"
-        height="700px"
-        title="Homekynd view"
-      ></iframe>
-    </div>
-  );
 }
